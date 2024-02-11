2f129d7696b6b7f7ee9266a709522a754b768e99
Magans11 <159688383+Magans11@users.noreply.github.com>
2024-02-11 21:54:01 +0700

    Initial commit

---
+++ Assets/Assets/Quiz/Scripts/Play.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start () {
+        
+    }
+    public void scale(float scale) {
+        transform.localScale = new Vector2(1 / scale, 1 * scale);
+    }
+    public void scane(string scane) {
+        Application.LoadLevel(scane);
+    }
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
+
